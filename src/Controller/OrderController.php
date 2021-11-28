87b4c09c2a6781e2ee76904db30fa4a4a8fd36a6
AlexV <vgzalexis@gmail.com>
2021-11-28 19:41:42 +0100

    feat: stripe

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -7,6 +7,8 @@ use App\Entity\Order;
 use App\Entity\OrderDetails;
 use App\Form\OrderType;
 use DateTimeImmutable;
+use Stripe\Checkout\Session;
+use Stripe\Stripe;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,20 +18,20 @@ use Symfony\Component\Routing\Annotation\Route;
 class OrderController extends AbstractController
 {
     private $entityManager;
-    public function __construct(EntityManagerInterface $entityManager){
+    public function __construct(EntityManagerInterface $entityManager)
+    {
         $this->entityManager = $entityManager;
     }
-    
+
     /**
      * @Route("/commande", name="order")
      */
     public function index(Cart $cart, Request $request): Response
     {
-        if (!$this->getUser()->getAdresses()->getValues())
-        {
+        if (!$this->getUser()->getAdresses()->getValues()) {
             return $this->redirectToRoute('account_adress_add');
         }
-        $form = $this->createForm( OrderType::class, null, [
+        $form = $this->createForm(OrderType::class, null, [
             'user' => $this->getUser()
         ]);
 
@@ -51,26 +53,26 @@ class OrderController extends AbstractController
      */
     public function add(Cart $cart, Request $request): Response
     {
-        $form = $this->createForm( OrderType::class, null, [
+        $form = $this->createForm(OrderType::class, null, [
             'user' => $this->getUser()
         ]);
 
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
             $date = new DateTimeImmutable();
             $carriers = $form->get('carriers')->getData();
             $delivery = $form->get('addresses')->getData();
-            $delivery_content = $delivery->getfirstName().''.$delivery->getlastName();
-            $delivery_content .= '<br/>'.$delivery->getPhone();
+            $delivery_content = $delivery->getfirstName() . '' . $delivery->getlastName();
+            $delivery_content .= '<br/>' . $delivery->getPhone();
 
-            if ($delivery->getCompany()){
-                $delivery_content .= '<br/>'.$delivery->getCompany();
+            if ($delivery->getCompany()) {
+                $delivery_content .= '<br/>' . $delivery->getCompany();
             }
 
-            $delivery_content .= '<br/>'.$delivery->getAddress();
-            $delivery_content .= '<br/>'.$delivery->getPostal().' '.$delivery->getCity();
-            $delivery_content .= '<br/>'.$delivery->getCompany();
+            $delivery_content .= '<br/>' . $delivery->getAddress();
+            $delivery_content .= '<br/>' . $delivery->getPostal() . ' ' . $delivery->getCity();
+            $delivery_content .= '<br/>' . $delivery->getCompany();
 
             //enregistrer ma commande Order()
             $order = new Order();
@@ -84,7 +86,7 @@ class OrderController extends AbstractController
             $this->entityManager->persist($order);
 
             //enregistrer ma commande OrderDetails()
-            foreach ($cart->getFull() as $product){
+            foreach ($cart->getFull() as $product) {
                 $orderDetails = new OrderDetails();
                 $orderDetails->setMyOrder($order);
                 $orderDetails->setProduct($product['product']->getName());
@@ -94,7 +96,33 @@ class OrderController extends AbstractController
                 $this->entityManager->persist($orderDetails);
             }
 
-            $this->entityManager->flush();
+            // $this->entityManager->flush();
+
+            Stripe::setApiKey('sk_test_51JyjlpAD1MNtxpfLZXRoZNsVoFyeovQzu0oJBh9GdpjTnY3RaQ7gEMb8ezS2IyqtdGU7XDcOa9ubI30my7ISvYqw00Os9aul88');
+            $YOUR_DOMAIN = 'http://localhost:127.0.0.1:8000';
+
+            $checkout_session = Session::create([
+
+                'line_items' => [[
+
+                    # Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
+
+                    'price' => '{{PRICE_ID}}',
+
+                    'quantity' => 1,
+
+                ]],
+
+                'mode' => 'payment',
+
+                'success_url' => $YOUR_DOMAIN . '/success.html',
+
+                'cancel_url' => $YOUR_DOMAIN . '/cancel.html',
+
+            ]);
+
+            dump($checkout_session->id);
+            dd($checkout_session);
 
             return $this->render('order/add.html.twig', [
                 'cart' => $cart->getFull(),
@@ -103,6 +131,6 @@ class OrderController extends AbstractController
             ]);
         }
 
-       return $this->redirectToRoute('cart');
+        return $this->redirectToRoute('cart');
     }
 }
