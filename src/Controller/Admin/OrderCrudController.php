ee39bf104ffa7b75a24331de379fc8689231124b
AlexV <vgzalexis@gmail.com>
2021-11-30 15:18:45 +0100

    feat: buttonlivraison

--- src/Controller/Admin/OrderCrudController.php
+++ src/Controller/Admin/OrderCrudController.php
@@ -3,16 +3,30 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Order;
+use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
-use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Router\CrudUrlGenerator;
 
 class OrderCrudController extends AbstractCrudController
 {
+    private $entityManager;
+    private $crudUrlGenerator;
+
+    public function __construct(EntityManagerInterface $entityManager, CrudUrlGenerator $crudUrlGenerator)
+    {
+        $this->entityManager = $entityManager; 
+        $this->crudUrlGenerator = $crudUrlGenerator;
+    }
+
     public static function getEntityFqcn(): string
     {
         return Order::class;
@@ -20,10 +34,44 @@ class OrderCrudController extends AbstractCrudController
 
     public function configureActions(Actions $actions): Actions
     {
+        $updatePreparation = Action::new('updatePreparation', 'Préparation en cours')->linkToCrudAction('updatePreparation');
+        $updatedelivery = Action::new('updatedelivery', 'Livraison en cours')->linkToCrudAction('updatedelivery');
+
         return $actions
+        ->add('detail', $updatePreparation)
+        ->add('detail', $updatedelivery)
         ->add('index', 'detail');
     }
 
+    public function updatePreparation(AdminContext $context)
+    {
+        $order = $context->getEntity()->getInstance();
+        $order->setState(2);
+        $this->entityManager->flush();
+
+        $this->addFlash('notice', "<span style='color:green;'><strong>La commande est bien <u> en cour de préparation </u></strong></span>");
+
+        $url = $this->crudUrlGenerator->build()
+            ->setController(OrderCrudController::class)
+            ->setAction('index')
+            ->generateUrl();
+        return $this->redirect($url);
+    }
+    
+    public function updateDelivery(AdminContext $context)
+    {
+        $order = $context->getEntity()->getInstance();
+        $order->setState(3);
+        $this->entityManager->flush();
+
+        $this->addFlash('notice', "<span style='color:blue;'><strong>La commande est bien <u> en cour de livraison </u></strong></span>");
+
+        $url = $this->crudUrlGenerator->build()
+            ->setController(OrderCrudController::class)
+            ->setAction('index')
+            ->generateUrl();
+        return $this->redirect($url);
+    }
     
     public function configureFields(string $pageName): iterable
     {
@@ -31,8 +79,15 @@ class OrderCrudController extends AbstractCrudController
             IdField::new('id'),
             DateField::new('createdAt', 'Passée le'),
             TextField::new('user.getFullName', 'Utilisateur'),
+            TextEditorField::new('delivery', 'Adresse de livraison')->onlyOnDetail(),
             MoneyField::new('total')->setCurrency('EUR'),
-            BooleanField::new('isPaid', 'Payée')
+            ChoiceField::new('state')->setChoices([
+                'Non payée' => 0,
+                'Payée' => 1,
+                'Préparation en cours' => 2,
+                'Livraison en cours' => 3
+            ]),
+
         ];
     }
     
--- src/Entity/Order.php
+++ src/Entity/Order.php
@@ -52,9 +52,9 @@ class Order
     private $orderDetails;
 
     /**
-     * @ORM\Column(type="boolean")
+     * @ORM\Column(type="integer")
      */
-    private $isPaid;
+    private $state;
 
     public function __construct()
     {
@@ -164,14 +164,14 @@ class Order
         return $this;
     }
 
-    public function getIsPaid(): ?bool
+    public function getState(): ?int
     {
-        return $this->isPaid;
+        return $this->state;
     }
 
-    public function setIsPaid(bool $isPaid): self
+    public function setState(int $state): self
     {
-        $this->isPaid = $isPaid;
+        $this->state = $state;
 
         return $this;
     }
