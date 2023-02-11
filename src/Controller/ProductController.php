747617710e7dd5cf30c8403b12bb0c25348d76bc
Bulcki <vgzalexis@gmail.com>
2023-02-11 11:51:54 +0100

    feat: test-commit

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -10,6 +10,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
 
 class ProductController extends AbstractController
 {
@@ -61,3 +62,27 @@ class ProductController extends AbstractController
         ]);
     }
 }
+
+// class ProductController extends AbstractController
+// {
+//     /**
+//      * @Route("/product", name="create_product")
+//      */
+//     public function createProduct(ManagerRegistry $doctrine): Response
+//     {
+//         $entityManager = $doctrine->getManager();
+
+//         $product = new Product();
+//         $product->setName('name');
+//         $product->setPrice('price');
+//         $product->setDescription('Description');
+
+//         // tell Doctrine you want to (eventually) save the Product (no queries yet)
+//         $entityManager->persist($product);
+
+//         // actually executes the queries (i.e. the INSERT query)
+//         $entityManager->flush();
+
+//         return new Response('Saved new product with id '.$product->getId());
+//     }
+// }
